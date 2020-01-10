738481eb96277c96569b34651ea87247af5083f6
kamlesh-00 <mistrykamlesh312@gmail.com>
2020-01-10 20:37:53 +0530

    Machine Learning Coursera

---
+++ costFunction.m
@@ -0,0 +1,8 @@
+function J = costFunction(X,y,theta)
+
+m=size(X,1);
+prediction=X*theta;
+
+sqrError=(prediction-y).^2;
+
+J=1/(2*m) * sum(sqrError);
---
+++ squarethis.m
@@ -0,0 +1,3 @@
+function [y1,y2] = squarethis(x)
+y1=x^2;
+y2=x^3;
